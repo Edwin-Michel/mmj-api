dd7a47f06d69384187678096415249a59933b1b3
Edwin Vargas <edwinn.dev@gmail.com>
2021-11-21 03:52:48 -0500

    Agregando permisos para Angular

--- src/main/java/com/edmi/iot/controller/IndicatorController.java
+++ src/main/java/com/edmi/iot/controller/IndicatorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200/"})
 @RequestMapping(value = "/ind")
 public class IndicatorController {
     @Autowired
